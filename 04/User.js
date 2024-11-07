afc3d80ac831dfd90f19985436bc933558f62ea8
Paweł Suder <pawel.suder94@gmail.com>
2024-11-07 19:39:34 +0100

    task 04 done

--- 04/User.js
+++ 04/User.js
@@ -1,3 +1,32 @@
 export default class User {
+  constructor(object) {
+    const { email, password } = object;
 
+    if (!email.includes("@")) {
+      this.throwError('email must have @"');
+    }
+
+    if (password.length < 8) {
+      this.throwError("password must be longer than 8 characters");
+    }
+
+    this.email = email;
+    this.password = password;
+  }
+
+  getEmail() {
+    return this.email;
+  }
+
+  getPassword() {
+    return this.password;
+  }
+
+  login() {
+    return this.email.includes("devmentor.pl");
+  }
+
+  throwError = (error) => {
+    throw new Error(error);
+  };
 }
