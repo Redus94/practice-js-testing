3b7f22631aba8e985bfc23d91d9a2a2312d1a26b
Paweł Suder <pawel.suder94@gmail.com>
2024-11-07 18:23:26 +0100

    the second test has been created

--- 03/app.test.js
+++ 03/app.test.js
@@ -3,3 +3,7 @@ import randomNumber from "./app";
 it("return 1 if min=1 & max=1", () => {
   expect(randomNumber(1, 1)).toBe(1);
 });
+
+it("throw error if min is not number", () => {
+  expect(() => randomNumber("1", 1)).toThrow("min is not a number");
+});
