60b6dc9c2565543f6de891b1336fdf52274a372a
Paweł Suder <pawel.suder94@gmail.com>
2024-11-07 19:02:49 +0100

    fourth test created

--- 03/app.test.js
+++ 03/app.test.js
@@ -11,3 +11,7 @@ it("throw error if min is not number", () => {
 it("throw error if max is not number", () => {
   expect(() => randomNumber(1, "1")).toThrow("max is not a number");
 });
+
+it("throw an error if min is greater than max", () => {
+  expect(() => randomNumber(2, 1)).toThrow("max is greater than min");
+});
