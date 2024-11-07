3cb3ba55e5b60e44595be88475fc262a9b1f0de7
Paweł Suder <pawel.suder94@gmail.com>
2024-11-07 18:31:45 +0100

    the third test works

--- 03/app.js
+++ 03/app.js
@@ -1,6 +1,11 @@
 export default function randomNumber(min, max) {
+
   if (typeof min !== "number") {
     throw new Error("min is not a number");
+  }
+
+  if (typeof max !== "number") {
+    throw new Error("max is not a number");
   }
   return Math.random() * (max - min) + min;
 }
