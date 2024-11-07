9519926c759143c865ecb4a72babe447e34fedb4
Paweł Suder <pawel.suder94@gmail.com>
2024-11-07 18:40:18 +0100

    refactored the third test

--- 03/app.js
+++ 03/app.js
@@ -1,11 +1,18 @@
 export default function randomNumber(min, max) {
-
-  if (typeof min !== "number") {
+  if (!isNumber(min)) {
     throw new Error("min is not a number");
   }
 
-  if (typeof max !== "number") {
-    throw new Error("max is not a number");
+  if (!isNumber(max)) {
+    throwError("max is not a number");
   }
   return Math.random() * (max - min) + min;
 }
+
+const throwError = (error) => {
+  throw new Error(error);
+};
+
+const isNumber = (value) => {
+  return typeof value === "number" && !Number.isNaN(value);
+};
